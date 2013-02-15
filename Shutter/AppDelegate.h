bb921f09143ea6f4ee8c275252e3f4ef7857766e
Shubham <shubham.goel@gmail.com>
2013-02-15 17:57:26 +0530

    Initial Commit

---
+++ Shutter/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Shutter
+//
+//  Created by Shubham Goel on 15/02/13.
+//  Copyright (c) 2013 Shubham Goel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Shutter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Shutter
+//
+//  Created by Shubham Goel on 15/02/13.
+//  Copyright (c) 2013 Shubham Goel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
